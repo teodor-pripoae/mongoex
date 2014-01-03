ed316b11ce35754f227fd60b0e2b71f8c4810c57
Lucas Allan Amorim <lucas.allan@gmail.com>
2014-01-03 13:45:39 -0800

    Added real tests cases

--- test/mongoex_test.exs
+++ test/mongoex_test.exs
@@ -4,8 +4,25 @@ Code.require_file "../fixtures/user.exs", __FILE__
 defmodule MongoexTest do
   use ExUnit.Case
 
+  setup_all do
+    Mongoex.Server.setup(address: '127.0.0.1', port: 27017, database: :your_app)
+    Mongoex.Server.start
+    :ok
+  end
+
   test "the truth" do
     assert true
+  end
+
+  test 'creates a new user record' do
+    user = User.new(name: "skywalker", age: 25).save
+    refute(nil == user._id)
+  end
+
+  test 'sets the fields properly' do
+    user = User.new(name: "skywalker", age: 25)
+    assert(user.name == "skywalker")
+    assert(user.age == 25)
   end
 
 end
